4c07b0b4768f1b863c0d79501b183608da258163
seiyasatouGit <114926045+seiyasatouGit@users.noreply.github.com>
2022-11-25 22:04:33 +0900

    first commit

---
+++ js/common.js
@@ -0,0 +1,17 @@
+$(function(){
+    AOS.init();
+
+    $(".main-visual").vegas({
+        timer: false,
+        transition: 'blur',
+        transitionDuration: 2000,
+        delay: 10000,
+        animation: 'kenburns',
+        animationDuration: 10000,
+        slides: [
+            { src: "./img/photo01.jpg" },
+            { src: "./img/photo011.jpg" },
+            { src: "./img/photo012.jpg" },
+        ]
+    });
+});
